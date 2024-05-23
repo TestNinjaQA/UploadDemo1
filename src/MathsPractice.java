51bf4ee3aff7f6c5c5aa51ba0d92a02b1e4368c2
TestNinjaQA <abhishek_rana@hotmail.com>
2024-05-23 19:45:27 +0530

    first commit

---
+++ src/MathsPractice.java
@@ -0,0 +1,14 @@
+
+public class MathsPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int A= Integer.parseInt(args[0]);
+    int B= Integer.parseInt(args[1]);
+    int Result;
+    Result=A+B;
+    System.out.println("Addition:"+Result);
+    
+	}
+
+}
---
+++ src/SquareArea.java
@@ -0,0 +1,14 @@
+
+public class SquareArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=5;
+       int b=10;
+       System.out.println("Area of Square:"+(a*a));
+       System.out.println("Perimeter of a Square:"+(4*a));
+       System.out.println("Area of Ractangle:"+(a*b));
+       System.out.println("Perimeter of Rectangle:"+(1/2*a+b));
+	}
+
+}
